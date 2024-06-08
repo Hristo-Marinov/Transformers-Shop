573d92a9f626862394f267777448a4e49af3d0f5
Hristo-Marinov <hristomarinov110506@gmail.com>
2024-06-08 08:49:46 +0300

    Delete useless stuff

--- TransformersShop/TransformersShop.ViewModels/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TransformersShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
